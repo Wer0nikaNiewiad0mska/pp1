d4adec991c3e4b8e81e93a5055ce9d7a46cd2269
Wer0nikaNiewiad0mska <146941847+Wer0nikaNiewiad0mska@users.noreply.github.com>
2023-10-24 19:13:06 +0200

    Add files via upload

--- 03-ControlStructures/29.py
+++ 03-ControlStructures/29.py
@@ -1,19 +1,20 @@
-my_list=[]
+washing_thing = input('What do you want to wash? jacket/underwear/shoes: ')
+spin=input('Do you want to spin it? y/n: ')
+rinse=input('Do you want to rinde it? y/n: ')
 
-washing_thing=input('What do you want to wash? (shoes, jacket, underwear):')
-r=input('Do yo want to rinse? yes/no:')
-s=input('Do you want to spin? yes/no:')
+if washing_thing == 'jacket':
+    time=40
+    time+=(9 if spin=='y' else 0)
+    time+=(15 if rinse=='y' else 0)
 
-shoes=20
-jacket=40
-underwear=70
+if washing_thing == 'shoes':
+    time=20
+    time+=(9 if spin=='y' else 0)
+    time+=(15 if rinse=='y' else 0)
 
-if washing_thing=='shoes':
-    if r=='yes':
-    my_list.append(r)
-    r=True
-    else:
-    r=False
+if washing_thing == 'underwear':
+    time=70
+    time+=(9 if spin=='y' else 0)
+    time+=(15 if rinse=='y' else 0)
 
-
-
+print(f'Total washing time: {time} minutes')
